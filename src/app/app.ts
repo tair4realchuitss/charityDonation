0e8c43223076072ee1a1ca5df1847fe4c8576025
Inkar <inkarfarkhatkyzy@gmail.com>
2026-04-18 18:58:28 +0500

    overall look of the web-site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,32 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
-  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('charity-donation-platform');
+export class AppComponent {
+
+  isLoggedIn = false;
+
+  constructor(private router: Router) {
+    const saved = localStorage.getItem('theme');
+    if (saved) document.body.className = saved;
+  }
+
+  login() {
+    this.isLoggedIn = true;
+  }
+
+  goProfile() {
+    this.router.navigate(['/user', 1]);
+  }
+
+  toggleTheme() {
+    const current = document.body.className === 'dark' ? 'light' : 'dark';
+    document.body.className = current;
+    localStorage.setItem('theme', current);
+  }
 }
