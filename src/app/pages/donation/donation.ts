1a3c8332769bdb4ef79c165ef6646dbf2c9f7aa4
Inkar <inkarfarkhatkyzy@gmail.com>
2026-04-18 19:10:24 +0500

    adding more pages

---
+++ src/app/pages/donation/donation.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donation',
+  imports: [],
+  templateUrl: './donation.html',
+  styleUrl: './donation.css',
+})
+export class Donation {}
